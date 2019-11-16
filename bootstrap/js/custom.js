fcf39a3c1fcf2a33daebe9a09c643abee3c91e8b
santosh-lame <suntoshvhajuu@gmail.com>
2019-11-16 16:28:07 +0545

    changed made

--- bootstrap/js/custom.js
+++ bootstrap/js/custom.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
       resolution: 'standard_resolution',
       accessToken: '7212983624.1677ed0.371dc2f7a25b4b5aa6dfade12cb2c0b7',
       sortBy: 'most-recent',
-      template: '<div class="activity col-xs-6 col-sm-6 col-md-3"><div class="img36_div"><a href="{{image}}"><img src="{{image}}" class="img-fluid" id="img-responsive2"/></a><div style="padding:10px;"><span style="padding-right:20px;" class="likes"><i class="fa fa-heart" aria-hidden="true"></i>{{likes}}</span><span style="padding-left:20px;" class="comments"><i class="fa fa-comments"aria-hidden="true"></i> {{comments}}</span></div></div></div>',
+      template: '<div class="activity col-xs-12 col-sm-6 col-md-3"><div class="img36_div"><a href="{{image}}"><img src="{{image}}" class="img-fluid" id="img-responsive2"/></a></div><div class="for_insta_info"><span style="padding-right:20px;" class="likes"><i class="fa fa-heart" aria-hidden="true"></i>{{likes}}</span><span style="padding-left:20px;" class="comments"><i class="fa fa-comments"aria-hidden="true"></i> {{comments}}</span></div></div>',
     });
       userFeed.run();
 
